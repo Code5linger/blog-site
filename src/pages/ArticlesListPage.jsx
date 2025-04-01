83c9bcd8db5a575bfd6738391d0290edceda8e86
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2025-04-01 22:06:55 +0600

    02.07-creating_and_linking_the_articles_list

--- src/pages/ArticlesListPage.jsx
+++ src/pages/ArticlesListPage.jsx
@@ -1,5 +1,20 @@
+import { Link } from 'react-router-dom';
+import articles from './article-content';
+
 const ArticleListPage = () => {
-  return <h1>ArticleList Page</h1>;
+  return (
+    <>
+      <h1>Articles</h1>
+      {articles.map((article, index) => (
+        <div key={index}>
+          <Link className="article-list-item" to={`/articles/${article.name}`}>
+            <h3>{article.title}</h3>
+            <p>{article.content[0].substring(0, 150)}...</p>
+          </Link>
+        </div>
+      ))}
+    </>
+  );
 };
 
 export default ArticleListPage;
