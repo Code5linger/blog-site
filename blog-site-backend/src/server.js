6707b6355682fd686383165d667d49ed1763f349
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2025-04-02 18:17:58 +0600

    04.05-rewriting_your_comments_endpoint

--- blog-site-backend/src/server.js
+++ blog-site-backend/src/server.js
@@ -1,34 +1,11 @@
 import express from 'express';
-import { MongoClient } from 'mongodb';
-
-// let articlesInfo = [
-//   {
-//     name: 'learn-react',
-//     upvotes: 68,
-//     comments: [],
-//   },
-//   {
-//     name: 'learn-node',
-//     upvotes: 0,
-//     comments: [],
-//   },
-//   {
-//     name: 'my-thoughts-on-resumes',
-//     upvotes: 0,
-//     comments: [],
-//   },
-// ];
+import { db, connectToDb } from './db.js'; // Explicit extension
 
 const app = express();
 app.use(express.json());
 
 app.get('/api/articles/:name', async (req, res) => {
   const { name } = req.params;
-  const client = new MongoClient('mongodb://127.0.0.1:27017');
-
-  await client.connect();
-
-  const db = client.db('react-blog-db');
 
   const article = await db.collection('articles').findOne({ name });
 
@@ -39,14 +16,19 @@ app.get('/api/articles/:name', async (req, res) => {
   }
 });
 
-app.post('/api/articles/:name/comments', (req, res) => {
+app.post('/api/articles/:name/comments', async (req, res) => {
   const { name } = req.params;
   const { postedBy, text } = req.body;
 
-  const article = articlesInfo.find((a) => a.name === name);
+  await db.collection('articles').updateOne(
+    { name },
+    {
+      $push: { comments: { postedBy, text } },
+    }
+  );
+  const article = await db.collection('articles').findOne({ name });
 
   if (article) {
-    article.comments.push({ postedBy, text });
     res.send(article.comments);
   } else {
     res.send("That article dosen't exist.");
@@ -56,10 +38,6 @@ app.post('/api/articles/:name/comments', (req, res) => {
 app.put('/api/articles/:name/upvote', async (req, res) => {
   const { name } = req.params;
 
-  const client = new MongoClient('mongodb://127.0.0.1:27017');
-  await client.connect();
-
-  const db = client.db('react-blog-db');
   await db.collection('articles').updateOne(
     { name },
     {
@@ -69,13 +47,16 @@ app.put('/api/articles/:name/upvote', async (req, res) => {
   const article = await db.collection('articles').findOne({ name });
 
   if (article) {
-    article.upvotes += 1;
     res.send(`The ${name} article now has ${article.upvotes} upvotes❗`);
   } else {
     res.send("That article dosen't exist.");
   }
 });
 
-app.listen(8000, () => {
-  console.log('👂 Port 8000');
+connectToDb(() => {
+  console.log('Test');
+
+  app.listen(8000, () => {
+    console.log('👂 Port 8000');
+  });
 });
