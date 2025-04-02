0473873a452b012ab9cc035b9dd98914fc0dfa9c
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2025-04-02 12:32:06 +0600

    03.07-adding_comments

--- blog-site-backend/src/server.js
+++ blog-site-backend/src/server.js
@@ -4,28 +4,35 @@ let articlesInfo = [
   {
     name: 'learn-react',
     upvotes: 68,
+    comments: [],
   },
   {
     name: 'learn-node',
     upvotes: 0,
+    comments: [],
   },
   {
     name: 'my-thoughts-on-resumes',
     upvotes: 0,
+    comments: [],
   },
 ];
 
 const app = express();
 app.use(express.json());
 
-// app.post('/hello', (req, res) => {
-//   res.send(`Hello ${req.body.name} 👋!!`);
-// });
+app.post('/api/articles/:name/comments', (req, res) => {
+  const { name } = req.params;
+  const { postedBy, text } = req.body;
 
-// app.get('/hello/:name', (req, res) => {
-//   const { name } = req.params;
-//   res.send(`Hello ${name}❗`);
-// });
+  const article = articlesInfo.find((a) => a.name === name);
+  if (article) {
+    article.comments.push({ postedBy, text });
+    res.send(article.comments);
+  } else {
+    res.send("That article dosen't exist.");
+  }
+});
 
 app.put('/api/articles/:name/upvote', (req, res) => {
   const { name } = req.params;
