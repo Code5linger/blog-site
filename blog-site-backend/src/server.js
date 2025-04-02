abcc8904b678b33982118d2fcc6f769968979971
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2025-04-02 11:14:01 +0600

    !

--- blog-site-backend/src/server.js
+++ blog-site-backend/src/server.js
@@ -4,8 +4,12 @@ const app = express();
 app.use(express.json());
 
 app.post('/hello', (req, res) => {
-  console.log(req.body);
   res.send(`Hello ${req.body.name} 👋!!`);
+});
+
+app.get('/hello/:name', (req, res) => {
+  const { name } = req.params;
+  res.send(`Hello ${name}❗`);
 });
 
 app.listen(8000, () => {
