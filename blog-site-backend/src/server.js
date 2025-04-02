0eaf75650bd0445942621220ba3eb4d1b8d5e14e
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2025-04-02 11:54:03 +0600

    03.05-upvoting_articles

--- blog-site-backend/src/server.js
+++ blog-site-backend/src/server.js
@@ -1,15 +1,41 @@
 import express from 'express';
 
+let articlesInfo = [
+  {
+    name: 'learn-react',
+    upvotes: 68,
+  },
+  {
+    name: 'learn-node',
+    upvotes: 0,
+  },
+  {
+    name: 'my-thoughts-on-resumes',
+    upvotes: 0,
+  },
+];
+
 const app = express();
 app.use(express.json());
 
-app.post('/hello', (req, res) => {
-  res.send(`Hello ${req.body.name} 👋!!`);
-});
+// app.post('/hello', (req, res) => {
+//   res.send(`Hello ${req.body.name} 👋!!`);
+// });
 
-app.get('/hello/:name', (req, res) => {
+// app.get('/hello/:name', (req, res) => {
+//   const { name } = req.params;
+//   res.send(`Hello ${name}❗`);
+// });
+
+app.put('/api/articles/:name/upvote', (req, res) => {
   const { name } = req.params;
-  res.send(`Hello ${name}❗`);
+  const article = articlesInfo.find((a) => a.name === name);
+  if (article) {
+    article.upvotes += 1;
+    res.send(`The ${name} article now has ${article.upvotes} upvotes`);
+  } else {
+    res.send("That article dosen't exist.");
+  }
 });
 
 app.listen(8000, () => {
