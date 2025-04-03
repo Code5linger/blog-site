993233f32ac5c7c4b9432f34a0cd62f0413356d2
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2025-04-03 20:15:19 +0600

    ⚓ ExpressJS

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -4,20 +4,24 @@ let articlesInfo = [
   {
     name: 'learn-react',
     upvotes: 5,
+    comments: [],
   },
   {
     name: 'learn-node',
     upvotes: 10,
+    comments: [],
   },
   {
     name: 'my-thoughts-on-resumes',
     upvotes: 15,
+    comments: [],
   },
 ];
 
 const app = express();
 app.use(express.json());
 
+// ! UPVOTE
 app.put('/api/articles/:name/upvote', (req, res) => {
   const { name } = req.params;
   const article = articlesInfo.find((article) => article.name === name);
@@ -29,8 +33,23 @@ app.put('/api/articles/:name/upvote', (req, res) => {
   }
 });
 
+// ! Comment
+app.post('/api/articles/:name/comments', (req, res) => {
+  const { name } = req.params;
+  const { postedBy, text } = req.body;
+
+  const article = articlesInfo.find((article) => article.name === name);
+
+  if (article) {
+    article.comments.push({ postedBy, text });
+    res.send(article.comments);
+  } else {
+    res.send("The article dosen't exist!");
+  }
+});
+
 app.listen(8000, () => {
-  console.log('✔️');
+  console.log('✔️✔️✔️✔️✔️✔️✔️✔️✔️✔️✔️✔️✔️✔️✔️✔️✔️✔️');
 });
 
 // GET => Load
