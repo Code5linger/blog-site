17326d49b341434808cd14ea61614dac217f4548
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2025-04-03 22:09:04 +0600

    ⚓ Database

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -1,32 +1,36 @@
 import express from 'express';
-
-let articlesInfo = [
-  {
-    name: 'learn-react',
-    upvotes: 5,
-    comments: [],
-  },
-  {
-    name: 'learn-node',
-    upvotes: 10,
-    comments: [],
-  },
-  {
-    name: 'my-thoughts-on-resumes',
-    upvotes: 15,
-    comments: [],
-  },
-];
+import { db, connectToDb } from './db.js';
 
 const app = express();
 app.use(express.json());
 
-// ! UPVOTE
-app.put('/api/articles/:name/upvote', (req, res) => {
+// ! Getting the POST
+app.get('/api/articles/:name', async (req, res) => {
   const { name } = req.params;
-  const article = articlesInfo.find((article) => article.name === name);
+
+  const article = await db.collection('articles').findOne({ name });
+
   if (article) {
-    article.upvotes += 1;
+    res.send(article);
+  } else {
+    res.sendStatus(404);
+  }
+});
+
+// ! UPVOTE
+app.put('/api/articles/:name/upvote', async (req, res) => {
+  const { name } = req.params;
+
+  await db.collection('articles').updateOne(
+    { name },
+    {
+      $inc: { upvotes: 1 },
+    }
+  );
+
+  const article = await db.collection('articles').findOne({ name });
+
+  if (article) {
     res.send(`The ${name} article now have ${article.upvotes} upvotes!`);
   } else {
     res.send("The article dosen't exist!");
@@ -34,24 +38,29 @@ app.put('/api/articles/:name/upvote', (req, res) => {
 });
 
 // ! Comment
-app.post('/api/articles/:name/comments', (req, res) => {
+app.post('/api/articles/:name/comments', async (req, res) => {
   const { name } = req.params;
   const { postedBy, text } = req.body;
 
-  const article = articlesInfo.find((article) => article.name === name);
+  await db.collection('articles').updateOne(
+    { name },
+    {
+      $push: { comments: { postedBy, text } },
+    }
+  );
+
+  const article = await db.collection('articles').findOne({ name });
 
   if (article) {
-    article.comments.push({ postedBy, text });
     res.send(article.comments);
   } else {
     res.send("The article dosen't exist!");
   }
 });
 
-app.listen(8000, () => {
-  console.log('✔️✔️✔️✔️✔️✔️✔️✔️✔️✔️✔️✔️✔️✔️✔️✔️✔️✔️');
+connectToDb(() => {
+  console.log('✅✅✅✅');
+  app.listen(8000, () => {
+    console.log('✔️✔️✔️✔️✔️✔️✔️✔️✔️✔️✔️✔️✔️✔️✔️✔️✔️✔️');
+  });
 });
-
-// GET => Load
-// POST => Create
-// PUT => Update
