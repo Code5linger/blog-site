3406a4e1b195b21e6c1301f9a1915599983a533e
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2025-04-04 21:05:11 +0600

    06.09-protecting_the_upvote_and_comment_endpoints

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -46,25 +46,40 @@ app.get('/api/articles/:name', async (req, res) => {
   }
 });
 
+app.use((req, res, next) => {
+  if (req.user) {
+    next();
+  } else {
+    res.sendStatus(401);
+  }
+});
+
 // ! UPVOTE
 app.put('/api/articles/:name/upvote', async (req, res) => {
   const { name } = req.params;
-
-  if (!req.user) {
-    return res.status(401).json({ error: 'Authentication required' });
-  }
-
-  await db.collection('articles').updateOne(
-    { name },
-    {
-      $inc: { upvotes: 1 },
-    }
-  );
-
+  const { uid } = req.user;
   const article = await db.collection('articles').findOne({ name });
 
   if (article) {
-    res.json(article);
+    const upvoteIds = article.upvoteIds || [];
+    const canUpvote = uid && !upvoteIds.includes(uid);
+
+    if (canUpvote) {
+      await db.collection('articles').updateOne(
+        { name },
+        {
+          $inc: { upvotes: 1 },
+          $push: { upvoteIds: uid },
+        }
+      );
+    }
+    const updatedArticle = await db.collection('articles').findOne({ name });
+
+    // if (!req.user) {
+    //   return res.status(401).json({ error: 'Authentication required' });
+    // }
+
+    res.json(updatedArticle);
   } else {
     res.send("The article dosen't exist!");
   }
@@ -73,7 +88,8 @@ app.put('/api/articles/:name/upvote', async (req, res) => {
 // ! Comment
 app.post('/api/articles/:name/comments', async (req, res) => {
   const { name } = req.params;
-  const { postedBy, text } = req.body;
+  const { text } = req.body;
+  const { email } = req.user;
 
   if (!req.user) {
     return res.status(401).json({ error: 'Authentication required' });
@@ -82,7 +98,7 @@ app.post('/api/articles/:name/comments', async (req, res) => {
   await db.collection('articles').updateOne(
     { name },
     {
-      $push: { comments: { postedBy, text } },
+      $push: { comments: { postedBy: email, text } },
     }
   );
 
